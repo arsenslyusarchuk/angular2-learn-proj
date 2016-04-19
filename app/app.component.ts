e2e3de747ad5f5f7ca193b15ccd18a73c9cc059d
Arsen Slyusarchuk <arsenslyusarchuk@perfectial.com>
2016-04-19 22:02:06 +0300

    Without routing

---
+++ app/app.component.ts
@@ -0,0 +1,45 @@
+import {Component} from 'angular2/core';
+import {OnInit} from 'angular2/core';
+import {Hero} from './hero';
+import {HeroDetailComponent} from './hero-detail.component';
+import {HeroService} from './hero.service';
+
+
+@Component({
+  selector: 'my-app',
+  directives: [HeroDetailComponent],
+  providers: [HeroService],
+  template:`
+    <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes items">
+      <li *ngFor="#hero of heroes"
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+  `
+})
+export class AppComponent implements OnInit {
+  title = 'Tour of Heroes';
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(private _heroService: HeroService) {
+    // constructor will assign values automatically
+  }
+  ngOnInit() {
+    // this.getHeroes();
+    this.getHeroesSlowly();
+  }
+  getHeroes() {
+    this._heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+  //2 sec delay
+  getHeroesSlowly() {
+    this._heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+  }
+  onSelect(hero: Hero) { this.selectedHero = hero; }
+}
